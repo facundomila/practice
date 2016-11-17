2dddf7902414997e225a8337eec83b5a30e96404
Facundo Mila <facundo.mila@wnco.com>
2016-11-17 14:59:58 -0600

    read and show data

--- simple-firebase/admin/js/login.js
+++ simple-firebase/admin/js/login.js
@@ -1,14 +1,27 @@
-var read = db.ref('users/facumila').on('value', function (snapshot) {
-    return name = snapshot.val()
+db.ref('noticias').on('value', function (snapshot) {
+    var noticias = snapshot.val();
+
+    renderNoticias(noticias);
 });
 
 var error = "Contraseña incorrecta";
 
-window.onload = function valid(){
-  if (name == "admin000") {
-    document.getElementById('login').innerHTML = 'pass: '+ name;
+window.onload = function (){
+
+  if (name == "facumila") {
+    document.getElementById('login').innerHTML = 'Bienvenido: '+ name;
   }
   else {
     document.getElementById('login').innerHTML = error;
-  };
+  }
+};
+
+function renderNoticias(noticias) {
+  document.getElementById('noticias').appendChild(document.createTextNode(''));
+
+  noticias.forEach(function (noticia) {
+    var node = document.createTextNode(noticia.titulo);
+
+    document.getElementById('noticias').appendChild(node);
+  });
 }
